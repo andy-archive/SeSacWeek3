53021e8b7257257c2c59fedaad4ad034f29b5f57
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-08-01 11:46:14 +0900

    Feat: [CustomTableViewController] 테이블 뷰 화면에 삭제 기능 및 업데이트 기능 구현

--- SeSacWeek3/Lectures/CustomTableViewController.swift
+++ SeSacWeek3/Lectures/CustomTableViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class CustomTableViewController: UITableViewController {
     
-    let todo = TodoInformation()
+    var todo = TodoInformation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,5 +35,20 @@ class CustomTableViewController: UITableViewController {
     //3. 셀 선택
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath)
+    }
+    
+    /// 제거 가능 상태 허용
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    /// 제거 ==> 업데이트
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        /// 1) 제거
+        todo.list.remove(at: indexPath.row)
+        
+        /// 2) 업데이트
+        tableView.reloadData()
     }
 }
